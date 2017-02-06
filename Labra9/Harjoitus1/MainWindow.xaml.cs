4875f3b7d57f919708f302e371cc5a32f3beee28
juuso <juuso@DESKTOP-H8ND6HS>
2017-02-06 12:25:39 +0200

    Lab 9 - Task 1 has been finished and commented

---
+++ Labra9/Harjoitus1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Tekijä: Juuso Huppunen
+ * AsioID: K8960
+ * Kurssi: TTOS0200
+ * Harjoitus: OOP Labra 9 - Tehtävä 1
+ * Kuvaus:
+ *		 Opiskelija on saanut itselleen unelma kesätyön vilkasliikenteisen kadun varrelta. 
+ *		 Hänen tulee laskea ohi menevät henkilö- ja kuorma-autot. Laadi opiskelijalle sovellus,
+ *		 jolla hän voi laskea helposti ohi menneet autot.
+ *		 Painikkeita painettaessa ko. kulkuneuvon osoittama lukumäärä kasvaa yhdellä. 
+ * Pvm: 31.1.2017
+ */
+
+using System;
+using System.Windows;
+
+namespace Harjoitus1 {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		private int trucs = 0;
+		private int cars = 0;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void truckButton_click(object sender, RoutedEventArgs e)
+		{
+			trucs++;
+			PrintTrucs.Text = Convert.ToString(trucs);
+		}
+
+		private void carbutton_click(object sender, RoutedEventArgs e)
+		{
+			cars++;
+			PrintCars.Text = Convert.ToString(cars);
+		}
+	}
+}
--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -8,7 +8,40 @@ namespace SandBox {
 	class Program {
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Tester();
+		}
+		static void Tester()
+		{
+			Test person1 = new Test("Jackson New", 45);
+			Console.WriteLine(person1.ToString());
+			person1.Age = -12;
+			Console.WriteLine(person1.ToString());
+
+		}
+	}
+	class Test {
+		public string Name { get; set; }
+		
+		private int age;
+		public int Age
+		{
+			get { return age; }
+			set {
+				age = value;
+				if (age < 0) {
+					age = 0;
+				}
+			}
+		}
+		public Test(string _name, int _age)
+		{
+			Name = _name;
+			Age = _age;
+		}
+		public override string ToString()
+		{
+			string tmp = string.Format("Name {0}, Age {1}", Name, Age);
+			return tmp;
 		}
 	}
 }
