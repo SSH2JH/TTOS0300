366223ed23e14a7f5fc8785dbb3a931afce5e3fa
juuso <juuso@DESKTOP-H8ND6HS>
2017-02-22 12:26:49 +0200

    Made some demos along with class

---
+++ Labra12/Demo1/Model/StudentModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Demo1.Model {
+	public class Student : INotifyPropertyChanged {
+		// Properties
+		private string firstname;
+		private string lastname;
+		private string asioid;
+		public string FirstName
+		{
+			get { return firstname; }
+			set {
+				if (firstname != value) {
+					firstname = value;
+					RaisePropertyChanged("FirstName");
+					RaisePropertyChanged("FullName");
+				}
+			}
+		}
+		public string LastName
+		{
+			get { return lastname; }
+			set {
+				if (lastname != value) {
+					lastname = value;
+					RaisePropertyChanged("LastName");
+					RaisePropertyChanged("FullName");
+				}
+			}
+		}
+		public string FullName {
+			get {
+				return firstname + " " + lastname;
+			}
+		}
+		public string AsioId
+		{
+			get { return asioid; }
+			set {
+				if (asioid != value) {
+					asioid = value;
+					RaisePropertyChanged("AsioId");
+				}
+			}
+		}
+		// Constructors
+		//-
+		// Methods
+		//.
+		// Events
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void RaisePropertyChanged(string property)
+		{
+			if (PropertyChanged != null) {
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
+		}
+	}
+}
---
+++ Labra12/Demo1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo1.Model;
+using System.Collections.ObjectModel;
+
+namespace Demo1.ViewModel {
+	class StudentViewModel {
+		public ObservableCollection<Student> Students {	get; set; }
+		public void LoadStudents()
+		{
+			ObservableCollection<Student> students = new ObservableCollection<Student>();
+			// Lisätään esimerkin vuoksi muutama opiskelija, oikeassa sovelluksessa haettaisiin tietokannasta
+			/*students.Add(new Student { FirstName = "Kalle", LastName = "Jalkanen", AsioId="A1234" });
+			students.Add(new Student { FirstName = "Teppo", LastName = "Testaaja", AsioId="A2345" });
+			students.Add(new Student { FirstName = "Tomi", LastName = "Tomminen", AsioId = "B1234" });
+			students.Add(new Student { FirstName = "Emilia", LastName = "Nieminen", AsioId = "B2345" });*/
+			Students = students;
+		}
+	}
+}
